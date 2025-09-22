50739c591addf92ac59cc6af282424d77dd78f4f
ma2345666 <mariaclarablaschi@gmail.com>
2025-09-22 13:29:40 -0300

    Add files via upload

--- 12_aula/atividade.sql
+++ 12_aula/atividade.sql
@@ -1,19 +1,15 @@
-use clinica_medica;
-select pacientes.nome,SUM(medicamentos.preco) as "gastos com medicamentos"
-from pacientes JOIN historicomedico
-ON pacientes.id= historicomedico.paciente_id JOIN prescricoes
+USE clinica_medica;
+-- RESOLVER O EXERCICIO 1
+-- ULTILIZAR A FUNÇAO SUM() PARA OMAR OS MEDICAMENTOS (PRECO).
+SELECT pacientes.nome, SUM(medicamentos.preco) as "Gastos com Medicamentos"
+FROM pacientes JOIN historicamedico
+ON pacientes.id = historicomedico.paciente_id JOIN prescricoes
 ON historicomedico.id = prescricoes.historico_medico_id JOIN medicamentos
-ON medicamentos.id = prescricoes.medicamento_id
+ON medicamentos.id = precricoes.medicamento_id
 GROUP BY pacientes.nome;
 
+-- MOSTRAR O TOTAL DE PRECRIÇÕES POR PACIENTE
+SELECT prescricoes.id "ID_Prescrição", medicamentos.nome "Medicamento"
+FROM pacientes JOIN medicamentos
+ON prescricoes.medicamento_id = medicamentos.id;
 
--- MOSTRAR O TOTAL DE PRESCRIÇÕES  POR PACIENTE
-select pacientes.nome,COUNT(prescricoes.id)
-from pacientes JOIN historicomedico
-ON pacientes.id= historicomedico.paciente_id JOIN prescricoes
-ON historicomedico.id = prescricoes.historico_medico_id JOIN medicamentos
-GROUP BY  pacientes.nome;
-
--- MOSTRAR OS MEDICAMENTOS DE CADA PRESCRIÇÃO"
-
-   
